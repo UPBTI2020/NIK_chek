0c101c81282fe23bade5dcc721956e9f6b367c96
codeaflaha <najamuddindwi@gmail.com>
2020-07-04 12:50:00 +0700

    upload program

---
+++ tes.js
@@ -0,0 +1,12 @@
+/*******************************************
+ * #### nik_parse.js ####
+ * Parse Nomor Induk Kependudukan (NIK) KTP
+ * Coded by @bachors 2018.
+ * https://github.com/bachors/nik_parse.js
+ * Updates will be posted to this site.
+ *******************************************/
+
+ const NIK = require('./src/nik_parse');
+
+ const result = NIK('3203012503770011')
+ console.log(result);
